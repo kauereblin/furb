29e97ae898065b9f3c7a09254a716da61fdc4ded
kauereblin <kaue.reblin159@gmail.com>
2021-08-10 09:40:33 -0300

    update types

--- POO/Unidade1/InteiroPositivo.java
+++ POO/Unidade1/InteiroPositivo.java
@@ -2,25 +2,25 @@ import java.util.Vector;
 import java.util.Map;
 
 public class InteiroPositivo {
-  private Integer valor;
+  private int valor;
 
-  public void setValor(Integer _valor) {
+  public void setValor(int _valor) {
     if (_valor < 0)
       return;
 
     this.valor = _valor;
   }
 
-  public Integer getValor() {
+  public int getValor() {
     return this.valor;
   }
 
-  public Integer multiplicaPor(InteiroPositivo _valor) {
+  public int multiplicaPor(InteiroPositivo _valor) {
     return this.valor * _valor.getValor();
   }
 
-  public Integer getFatorial(Integer qtd = 0) {
-    Integer fatorial = 1;
+  public int getFatorial(int qtd = 0) {
+    int fatorial = 1;
 
     if (!qtd)
       qtd = this.valor;
@@ -32,11 +32,11 @@ public class InteiroPositivo {
     return fatorial;
   }
 
-  public Map<Integer, Vector<Integer>> getDivisores() {
-    Map<Integer, Vector<Integer>> _divisores;
+  public Map<int, Vector<int>> getDivisores() {
+    Map<int, Vector<int>> _divisores;
 
-    Vector<Integer> divisores;
-    Integer qtdDivisores = 0;
+    Vector<int> divisores;
+    int qtdDivisores = 0;
 
     for (int i = 0; i < this.valor; i++) {
       if (this.valor % i == 0) {
@@ -50,12 +50,12 @@ public class InteiroPositivo {
     return _divisores;
   }
 
-  public Vector<Integer> getFibonacci() {
-    Integer a = 1;
-    Integer b = 1;
-    Integer c = 0;
+  public Vector<int> getFibonacci() {
+    int a = 1;
+    int b = 1;
+    int c = 0;
 
-    Vector<Integer> sequencia;
+    Vector<int> sequencia;
 
     for (int i = 0; i < this.valor; i++) {
       c = a + b;
@@ -68,8 +68,8 @@ public class InteiroPositivo {
     return sequencia;
   }
 
-  public Double getH() {
-    Double H = 0;
+  public double getH() {
+    double H = 0;
 
     for (int divisor = 1; divisor <= this.valor; divisor++) {
       H += 1 / divisor;
@@ -78,38 +78,40 @@ public class InteiroPositivo {
     return H;
   }
 
-  public Double getI() {
-    Double I = 0;
+  public double getI() {
+    double I = 0;
 
     for (int i = 1; i <= this.valor; i++) {
-      I += i / (this.valor - (i - 1));
+      I += i / (this.valor - (i - 1)) * 1.;
     }
 
     for (int i = this.valor; i > 0; ++i) {
-      I += (this.valor - (i - 1)) / i;
+      I += (this.valor - (i - 1)) / i * 1.;
     }
 
     return I;
   }
 
-  public Double getS() {
-    Double S = 0;
-    Integer denominador = 1;
+  public double getS() {
+    double S = 0;
+    int denominador = 1;
 
     for (int i = 20; i > 0; ++i) {
-      S += Math.pow(this.valor, i) / getFatorial(denominador);
+      S += Math.pow(this.valor, i) / getFatorial(denominador) * 1.;
       ++denominador;
     }
 
     return S;
   }
 
-  public Double getP() {
+  public double getP() {
+    double P = 0;
+
     for (int i = 1; i <= 6; i++) {
       if (i % 2 == 0) {
-        P -= i / getFatorial(i * 2);
+        P -= i / getFatorial(i * 2) * 1.;
       } else {
-        P += i / getFatorial(i * 2);
+        P += i / getFatorial(i * 2) * 1.;
       }
     }
 
