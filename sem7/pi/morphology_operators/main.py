3fb27ed26b425880cfaa9b4dd346a9e69f8cb3bc
kauereblin <kaue.reblin159@gmail.com>
2024-04-06 17:50:39 -0300

    adjust in threshold range

--- sem7/pi/morphology_operators/main.py
+++ sem7/pi/morphology_operators/main.py
@@ -6,12 +6,12 @@ INPUT_IMAGE = "mama6"
 
 if __name__ == "__main__":
     img = cv2.imread(f"./assets/{INPUT_IMAGE}.png", 0)
-    kernel = np.ones((5,5), np.uint8)
+    kernel = np.ones((5, 5), np.uint8)
 
     tophat = cv2.morphologyEx(img, cv2.MORPH_TOPHAT, kernel)
     plt.imshow(tophat, cmap='grey')
 
-    (T, bin) = cv2.threshold(tophat, (20 if (cv2.minMaxLoc(tophat)[1] - 100) <= 20 else (cv2.minMaxLoc(tophat)[1] - 100)), 255, cv2.THRESH_BINARY)
+    (T, bin) = cv2.threshold(tophat, 35, 255, cv2.THRESH_BINARY)
     plt.imshow(bin, 'gray')
     plt.title('Non-flat SE binary thresholded')
     plt.show()
